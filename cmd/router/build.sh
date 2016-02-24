1bf2b15f80ca307fcc114492e9ec84142b206b81
Graham Anderson <graham.anderson@gmail.com>
2016-02-24 15:14:34 +0000

    cmd/router: basic main() wrapper for liberty balancer

---
+++ cmd/router/build.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+verbose='false'
+appenv=''
+push=false
+
+while getopts 'e:pv' flag; do
+  case "${flag}" in
+    e) appenv="${OPTARG}" ;;
+    p) push=true ;;
+    v) verbose='true' ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+[[ -z "$appenv" ]] && { echo "You must give supply the 'appenv' '-e' argument" ; exit 1; }
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
+go build .
+docker build --rm -t registry.golang.scot/router:${appenv} -f $DIR/${appenv}.Dockerfile $DIR
+
+if [ "$push" = true ] ; then
+	docker push registry.golang.scot/router:${appenv}
+fi
---
+++ cmd/router/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/golang/glog"
+	"golang.scot/pkg/liberty"
+)
+
+const (
+	defaultAddr = ":80"
+	svcName     = "router"
+)
+
+const (
+	modeDevelopment = iota
+	modeServing
+)
+
+var (
+	addr      = flag.String("addr", defaultAddr, "The address to bind to.")
+	cacheReqs = flag.Bool("cache", false, "Cache requests from backends.")
+)
+
+// print the usage help text for --help / -h
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		"\nUsage:\thttp-router [options]\n\n"+
+			"e.g.\thttp-router -addr="+defaultAddr+"\n\n"+
+			"Options\n"+
+			"-------\n\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = 100
+	balancer := liberty.NewBalancer()
+
+	glog.Infoln("Router is bootstrapped, listening for connections...")
+	if err := balancer.Balance(liberty.BestEffort); err != nil {
+		glog.Errorf("Fatal error starting load balancer: %s, %t\n", err, err)
+	}
+
+	// DO NOT REMOVE
+	glog.Flush()
+}
